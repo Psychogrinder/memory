7bcebb2b52c524880336f1c417a348f87aa1bd09
Psychogrinder <noirart@gmail.com>
2018-03-10 19:57:50 +0300

    Add main card script

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@ var cardsContainer = document.querySelector('.cards-container');
 var array1 = [0, 1, 2, 3, 4, 5, 6, 7];
 var array2 = [0, 1, 2, 3, 4, 5, 6, 7];
 var allCards = array1.concat(array2);
-
-
+var openCard1;
+var openCard2;
+var state = 0;
+var gameScore = 0;
 
 function shuffle(arr) {
   var j, x, i;
@@ -16,6 +18,43 @@ function shuffle(arr) {
   }
 }
 
+function openCard(card) {
+  card.classList.add('flipped');
+}
+
+function closeCard(card) {
+  card.classList.remove('flipped');
+}
+
+function areSameCards(firstCard, secondCard) {
+  if (firstCard.querySelector('.front').innerHTML == secondCard.querySelector('.front').innerHTML) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function handleClick(card) {
+  if (!card.classList.contains('flipped')) {
+    console.log('Closed');
+    if (state == 0) {
+      openCard(card);
+      openCard1 = card;
+      state = 1;
+    } else {
+      openCard(card);
+      openCard2 = card;
+      if (!areSameCards(openCard1, openCard2)) {
+        setTimeout(function(){
+          closeCard(openCard1);
+          closeCard(openCard2);
+        }, 500);
+      }
+      state = 0;
+    }
+  }
+}
+
 function createCards() {
   for(var i = 0; i < allCards.length; i++) {
     (function () {
@@ -23,7 +62,7 @@ function createCards() {
       cln.querySelector('.front').innerHTML = allCards[i];
       cardsContainer.appendChild(cln);
       cln.addEventListener('click', function(){
-      cln.classList.add('flipped');
+        handleClick(cln);
       });
     }());
   }
